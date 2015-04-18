ecc648e86bf38bd8239cd0e71d7ccaf17a08e942
KittenYang <kittenyang@icloud.com>
2015-04-18 21:36:14 +0800

    first commit

---
+++ KYPushTransitionDemo/KYPushTransitionDemo/KYPushTransition.m
@@ -0,0 +1,13 @@
+//
+//  KYPushTransition.m
+//  KYPushTransitionDemo
+//
+//  Created by Kitten Yang on 4/18/15.
+//  Copyright (c) 2015 Kitten Yang. All rights reserved.
+//
+
+#import "KYPushTransition.h"
+
+@implementation KYPushTransition
+
+@end
---
+++ KYPushTransitionDemo/KYPushTransitionDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KYPushTransitionDemo
+//
+//  Created by Kitten Yang on 4/18/15.
+//  Copyright (c) 2015 Kitten Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
